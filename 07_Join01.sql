e657bcf9b2a83f3d99d0e93cc8f7e1ecbbc38341
Afsarsoft <15915242+Afsarsoft@users.noreply.github.com>
2020-09-04 12:34:40 -0700

    ...

--- 07_Join01.sql
+++ 07_Join01.sql
@@ -1,40 +1,47 @@
--- Using AdventureWorks2019 DB
-USE AdventureWorks2019
+-- Using Northwind DB
+USE Northwind
 
 -- INNER JOIN (default join)
 -- Rows shared beween tables
-SELECT BusinessEntityID, FirstName, LastName
-FROM Person.Person;
+SELECT CustomerID, CompanyName, ContactName
+FROM dbo.Customers;
 
-SELECT BusinessEntityID, PhoneNumber
-FROM Person.PersonPhone
-WHERE BusinessEntityID = 293;
+SELECT OrderID, CustomerID, OrderDate
+FROM dbo.Orders
+WHERE CustomerID = 'ALFKI';
 
-SELECT A.BusinessEntityID
-      , A.FirstName
-   , A.LastName
-   , B.PhoneNumber
-FROM Person.Person AS A
+SELECT O.OrderID,
+   O.CustomerID,
+   C.CompanyName,
+   C.ContactName,
+   O.OrderDate
+FROM dbo.Orders AS O
    INNER JOIN
-   Person.PersonPhone AS B
-   ON A.BusinessEntityID = B.BusinessEntityID;
+   dbo.Customers AS C
+   ON O.CustomerID = O.CustomerID;
+-- (75530 rows affected)
 
 -- LEFT OUTER JOIN
 -- All rows from table left regradless of matching rows   
--- For not matching rows, we get NULL columns from left table  
-SELECT P.BusinessEntityID
-   , P.PersonType
-   , P.FirstName
-   , P.LastName
-   , E.JobTitle
-FROM Person.Person AS P
+-- For not matching rows, we get NULL columns from left table 
+-- https://tutorialdeep.com/sql/sql-left-outer-join/ 
+-- https://stackoverflow.com/questions/3308122/how-do-i-decide-when-to-use-right-joins-left-joins-or-inner-joins-or-how-to-dete/
+SELECT O.OrderID,
+   O.CustomerID,
+   C.CompanyName,
+   C.ContactName,
+   O.OrderDate
+FROM dbo.Customers AS C
    LEFT OUTER JOIN
-   HumanResources.Employee AS E
-   ON P.BusinessEntityID = E.BusinessEntityID;
+   dbo.Orders AS O
+   ON O.CustomerID = O.CustomerID;
+-- (75530 rows affected)
 
 -- RIGHT OUTER JOIN
 -- All rows from table right regradless of matching rows
 -- For not matching rows, we get NULL columns from right table  
+-- There is not much using the RIGHT Join. We can always do the LEFT join 
+-- https://stackoverflow.com/questions/436345/when-or-why-would-you-use-a-right-outer-join-instead-of-left
 
 -- FULL OUTER JOIN
 -- All rows from both tables regradless of matching rows
